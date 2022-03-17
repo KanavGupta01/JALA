4c67a303a6201f304a09f9843415ba5b9aa7cf06
Kanav Gupta <64695069+KanavGupta01@users.noreply.github.com>
2022-03-17 14:41:44 +0530

    Add more solutions

---
+++ C#.NET/8.For-Foreach-Loop/8.For-Foreach-Loop_2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _8.For_Foreach_Loop_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program in C# Sharp to display the n terms of odd natural number and their sum
+            int input = 0;
+            int sum = 0;
+
+            Console.Write("Enter number of natural terms you want: ");
+            input = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("The first Odd " + input + " natural numbers are:");
+
+            for (int i = 1; i <= input; i += 2)
+            {
+                sum += i;
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("The Sum of Odd Natural Number upto " + input + " terms is: " + sum);
+
+            // Wait for keyboard press before closing terminal window
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C#.NET/8.For-Foreach-Loop/8.For-Foreach-Loop_3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _8.For_Foreach_Loop_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // By using Array write the program using For and Foreach loop in c#
+            string[] skills = { "ASP.NET", "C#", "HTML", "CSS", "JavaScript", "SQL Server" };
+
+            foreach (string skill in skills)
+            {
+                Console.WriteLine(skill);
+            }
+
+            // Wait for keyboard press before closing terminal window
+            Console.ReadKey();
+        }
+    }
+}
